e9aacb1d27a8439999db902fb6a453490facf5d2
nargis192 <shaikhnargis194@gmail.com>
2024-09-15 21:41:43 +0530

    mongo url change

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/gameDB');
+    await mongoose.connect('mongodb+srv://shaikhnargis194:merngame@cluster0.lzwgu.mongodb.net/');
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
